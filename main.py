3581db2caef3cf3d25fdda4b2a342336495a14d0
Hsiao-Jung Su (蘇小容) <11111066@am990156638.local>
2025-03-21 21:04:29 +0800

    update: update json out string-3::

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ def get_sheet_data():
         print("🔍 型別:", type(creds_json))
         print("✅ GOOGLE_CREDENTIALS_JSON 已載入全部:",str(creds_json)[:-3])
         creds_json = str(creds_json)[:-3]+'"}'
-		print("✅ 已載入全部creds_json",str(creds_json)[::])
+        print("✅ 已載入全部creds_json",str(creds_json)[::])
+		
         if not creds_json:
             print("❌ GOOGLE_CREDENTIALS_JSON 環境變數未設定")
             raise Exception("❌ GOOGLE_CREDENTIALS_JSON 環境變數未設定")
