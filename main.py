182dd229dee302955f11a51d7782a5a6379bc652
Hsiao-Jung Su (蘇小容) <11111066@am990156638.local>
2025-03-28 11:40:04 +0800

    update: edit line message only the lyrics }}

--- main.py
+++ main.py
@@ -227,7 +227,7 @@ def handle_message(event):
         for row in records:
             if keyword in row.get("歌詞", ""):
                 #matched.append(TextSendMessage(text=f'{row["歌名"]} - {row["演唱者"]}\n{row["歌詞"]}'))
-                matched.append(TextSendMessage(text=f'{{row["歌詞"]}')) 
+                matched.append(TextSendMessage(text=f'{row["歌詞"]}'))
 
         # 如果有符合的歌詞，回覆最多 5 則（LINE API 單次最多 5 則訊息）
         if matched:
